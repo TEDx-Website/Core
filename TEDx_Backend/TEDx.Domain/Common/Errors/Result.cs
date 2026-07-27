f4f81f446aff320886358f1a276517c1bd6ee2ae
Your Name <ja8040637@gmail.com>
2026-07-27 14:57:35 +0300

    Implemeny Modular Monolith with Clean Architecture and Result Pattern

---
+++ TEDx_Backend/TEDx.Domain/Common/Errors/Result.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEDx.Domain.Common.Errors
+{
+    public sealed class Result<T>
+    {
+        public bool IsSuccess { get; } //
+        public T? Value { get; }
+        public Error? Error { get; }
+
+        private Result(bool isSuccess,T? value, Error? error)
+        {
+            IsSuccess = isSuccess;
+            Value = value;
+            Error = error;
+        }
+        public static Result<T> Success(T value)
+        {
+            return new Result<T>(
+                true,
+                value,
+                null);
+        }
+        public static Result<T> Failure(Error error)
+        {
+            return new Result<T>(
+                false,
+                default,
+                error);
+        }
+    }
+}
