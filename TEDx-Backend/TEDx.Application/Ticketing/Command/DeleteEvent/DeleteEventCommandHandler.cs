d48ac40444d39c873b5957ad00b218a56ec2aca1
Your Name <ja8040637@gmail.com>
2026-08-10 06:09:16 +0300

    Implement Delete Event EndPoint

---
+++ TEDx-Backend/TEDx.Application/Ticketing/Command/DeleteEvent/DeleteEventCommandHandler.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using TEDx.Application.Common.Interfaces;
+using TEDx.Domain.Common;
+using TEDx.Application.Common.Errors;
+using TEDx.Domain.Ticketing.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace TEDx.Application.Ticketing.Command.DeleteEvent
+{
+    internal class DeleteEventCommandHandler : IRequestHandler<DeleteEventCommand, Result<Unit>>
+    {
+        private readonly IAppDbContext _appDbContext;
+        private readonly IClock _clock;
+
+        public DeleteEventCommandHandler(IAppDbContext appDbContext, IClock clock)
+        {
+            _appDbContext = appDbContext;
+            _clock = clock;
+        }
+
+        public async Task<Result<Unit>> Handle(DeleteEventCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _appDbContext.Events.FirstOrDefaultAsync(e => e.Id == request.EventId, cancellationToken);
+            if (entity == null)
+            {
+                return Result<Unit>.Failure(Errors_Ticketing.EventNotFound);
+            }
+            var HasOrders = await _appDbContext.Orders.AnyAsync(o => o.EventId == request.EventId, cancellationToken);
+
+            if (HasOrders)
+            {
+                return Result<Unit>.Failure(Errors_Ticketing.EventHasOrders);
+            }
+            if(entity.Status == EventStatus.Published)
+            {
+                return Result<Unit>.Failure(Errors_Ticketing.HasOrdersCannotUnpublish);
+            }
+            entity.IsDeleted = true;
+            entity.DeletedAtUtc = _clock.UtcNow;
+
+            await _appDbContext.SaveChangesAsync(cancellationToken);
+
+            return Result<Unit>.Success(Unit.Value);
+        }
+    }
+}
